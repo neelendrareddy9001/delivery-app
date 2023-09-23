302ca9c839c3ff4cfb1e8c20dbae3de264391df9
neelendrareddy9001 <neelendrareddy123@gamil.com>
2023-09-23 21:35:07 +0200

    updated header with styles

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {} from "@mui/material";
+import {
+  BarChart,
+  SearchRounded,
+  ShoppingCartRounded,
+} from "@mui/icons-material";
+
+import HeaderImg from "../assets/header-img.png";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img src={HeaderImg} alt="header-img" className="logo" />
+
+        <div className="inputBox">
+          <SearchRounded className="searchIcon" />
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <div className="shoppingCart">
+          <ShoppingCartRounded className="cart" />
+          <div className="cart_content">
+            <p>2</p>
+          </div>
+        </div>
+
+        <div className="profileContainer">
+          <div className="imgBox">
+            <img src="" alt="" />
+          </div>
+
+          <h2 className="userName">John Doe</h2>
+        </div>
+
+        <div className="toggleMenu">
+          <BarChart className="toggleIcon" />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
