4af1b21ed0eacb5e14e925e6a68cc9228018ea8f
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-10-18 23:15:42 +0200

    banner compponent updated

--- src/components/BannerName.js
+++ src/components/BannerName.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const BannerName = ({ name, discount, link }) => {
+function BannerName({ name, discount, more }) {
+  const currency = "$";
   return (
-    <>
-      <div className="bannerContent">
-        <h3>Hello {name}</h3>
-        <p>
-          Get free discount for every <span>${discount}</span>
-        </p>
-        <a href={link}>Learn More</a>
-      </div>
-    </>
+    <div className="bannerContent">
+      <h3>Hello {name},</h3>
+      <p>
+        Get free discount for every <span>{`${currency}${discount}`}</span>{" "}
+        purchase
+      </p>
+      <a href={more}>Learn More</a>
+    </div>
   );
-};
+}
 
 export default BannerName;
